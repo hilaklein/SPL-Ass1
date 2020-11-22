155339f551db6159562b3c2a44ab97ab1fbb9665
paveltom <paveltom@post.bgu.ac.il>
2020-11-22 21:47:10 +0200

    done????????

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,11 @@
 using namespace std;
 
 int main(int argc, char** argv){
-    cout<<"starts here<==============" << endl;
     if(argc != 2){
         cout << "usage cTrace <config_path>" << endl;
         return 0;
     }
-    cout<<"calling Session" << endl;
-
     Session sess(argv[1]);
-    cout<<"main: next command -> 'sess.simulate();'" << endl;
     sess.simulate();
-
-    cout << "print this before return 0" << endl;
-
     return 0;
 }

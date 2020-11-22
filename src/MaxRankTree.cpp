c9f25e49c6cdb0cd1ce3c49299bfde3dd266dec0
paveltom <paveltom@post.bgu.ac.il>
2020-11-22 22:54:36 +0200

    done?????????

--- src/MaxRankTree.cpp
+++ src/MaxRankTree.cpp
@@ -3,15 +3,44 @@
 
 MaxRankTree::MaxRankTree(int rootLabel) : Tree(rootLabel) {}
 
+//int MaxRankTree::traceTree() {
+//    int output = node;
+//    if (children.empty())
+//        return output;
+//    int tempValue;
+//    for (Tree* tempTree:children){
+//        tempValue = tempTree->traceTree();
+//        if (output < tempValue)
+//            output = tempValue;
+//    }
+//    return output;
+//}
+
+//Tree* MaxRankTree::traceTree(Tree* curr) {
+//    if (curr->getChildren().size() == 0)
+//        return curr;
+//    Tree* out
+//    for (Tree* tempTree : curr->getChildren()){
+//
+//    }
+//
+//}
+
+
+
+
 int MaxRankTree::traceTree() {
     int output = node;
-    if (!children.empty())
-        return output;
+    if (children.empty())
+        return 0;
     int tempValue;
     for (Tree* tempTree:children){
-        tempValue = tempTree->traceTree();
-        if (output < tempValue)
-            output = tempValue;
+        tempValue = children.size();
+        if (tempTree->getChildren().size() > tempValue) {
+            tempValue = tempTree->getChildren().size();
+            output = tempTree->getNodeIndex();
+        }
+        tempTree->traceTree();
     }
     return output;
 }
--- src/Virus.cpp
+++ src/Virus.cpp
@@ -21,14 +21,16 @@ void Virus::act(Session &session) { // Animal
     }
     Virus *addV;
     vector<int> neighbors = tempGraph->getNeighbors(nodeInd); //receiving current virus neighbors
-    for (int ind : neighbors) { //running through curr virus neighbors to spread into one that was never infected
+    int sizeNeighbors = neighbors.size();
+    for (int i = 0; i < sizeNeighbors; i++) { //running through curr virus neighbors to spread into one that was never infected
         //do nor spread into yourself -> you are already infected + check that neighbor.at(i) was never infected
-        if (!tempGraph->isInfected(ind)) {
-            tempGraph->infectNode(ind); //mark neighbor as infected
-            addV = new Virus(ind);
+        if (!tempGraph->isInfected(neighbors.at(i))) {
+            tempGraph->infectNode(neighbors.at(i)); //mark neighbor as infected
+            addV = new Virus(neighbors.at(i));
             session.addAgent(*addV); //add neighbor as new agent
             //ind = session.numOfNodes; //break -> not needed after all
             delete addV; //cause addAgent creates a deep copy of 'addV'
+            i = sizeNeighbors;
         }
     }
     neighbors.clear();
